9a9ecc65d180f56511aa69850a2f1bf711924488
Loïc Kreitmann <loickreitmann@gmail.com>
2025-03-31 00:21:08 -0700

    added more to details and added the health route

--- src/app.py
+++ src/app.py
@@ -1,17 +1,26 @@
+import socket
 from flask import Flask, jsonify
+import datetime
 
 app = Flask(__name__)
 
 # '/api/v1/details'
 @app.route('/api/v1/details')
-
-# TODO healthz route
-# '/api/v1/healthz'
-
 def details():
     msg = 'Hello there, Porto!'
-    json_data = {"message": msg}
+    json_data = {
+        "message": msg,
+        "time": datetime.datetime.now().strftime("%H:%M:%S on %B %d, %Y"),
+        "hostname": socket.gethostname()
+    }
     return jsonify(json_data)
+
+# '/api/v1/healthz'
+@app.route('/api/v1/healthz')
+def health():
+    healthy_json = {"status": "up"}
+    healthy_code = 200
+    return jsonify(healthy_json), healthy_code
 
 if __name__ == '__main__':
     app.run()
