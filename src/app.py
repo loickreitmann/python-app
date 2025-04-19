d81c7c02062f0de857ab9b12ba8259cba587c9f0
Loïc Kreitmann <loickreitmann@gmail.com>
2025-04-19 16:54:08 -0700

    added the --skip-tls-test flag

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ def root():
 # '/<name>'
 @app.route("/<name>")
 def hello(name):
-    return f"Hello, {escape(name)}. What are you up to today? Don't you want to go home and rethink your life?"
+    return f"Hello, {escape(name)}. It's {datetime.datetime.now().strftime("%A, %B %d, %Y")}. Don't you want to go home and rethink your life?"
 
 # '/api/v1/details'
 @app.route('/api/v1/details')
