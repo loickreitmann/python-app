40f72a4254236f1a1dc5cbb397147bce465fa07f
Loïc Kreitmann <loickreitmann@gmail.com>
2025-04-19 15:29:12 -0700

    made a change to kick off a build

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ def root():
 # '/<name>'
 @app.route("/<name>")
 def hello(name):
-    return f"Hello, {escape(name)}. How are you today?"
+    return f"Hello, {escape(name)}. What are you up to today? "
 
 # '/api/v1/details'
 @app.route('/api/v1/details')
