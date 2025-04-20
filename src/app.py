d25444a697e894fd5e8f680ed8ad4dd5e7a45f15
Loïc Kreitmann <loickreitmann@gmail.com>
2025-04-19 17:21:41 -0700

    Ey, Inyalowda.

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # '/'
 @app.route("/")
 def root():
-    return f"Hello, there. It's {datetime.datetime.now().strftime("%A, %B %d, %Y")}. You want to go home and rethink your life."
+    return f"Ey, Inyalowda. Dis {datetime.datetime.now().strftime("%A, %B %d, %Y")}, ya. To ke go back home an pensa 'bout to laif, ya?"
 
 # '/<name>'
 @app.route("/<name>")
