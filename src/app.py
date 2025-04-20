c567d796e251aa180048164235cd8c4dd884d0ad
Loïc Kreitmann <loickreitmann@gmail.com>
2025-04-19 17:02:18 -0700

    Oye beratna

--- src/app.py
+++ src/app.py
@@ -18,7 +18,7 @@ def hello(name):
 # '/api/v1/details'
 @app.route('/api/v1/details')
 def details():
-    msg = 'Hello, there!'
+    msg = 'Oye beratna!'
     json_data = {
         "message": msg,
         "time": datetime.datetime.now().strftime("%H:%M:%S on %B %d, %Y"),
